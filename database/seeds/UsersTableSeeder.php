d5847c03e10b47d9e0215119d02e89d0be283fa9
Rahmat Awaludin <rahmat.awaludin@gmail.com>
2017-10-09 11:05:23 +0700

    initial

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Admin Ganteng',
+            'email' => 'admin@gmail.com',
+            'password' => bcrypt('rahasia'),
+            'level' => User::ACCESS_ADMIN
+        ]);
+
+        factory(User::class)->create([
+            'name' => 'Member Kece',
+            'email' => 'member@gmail.com',
+            'password' => bcrypt('rahasia'),
+            'level' => User::ACCESS_MEMBER
+        ]);
+    }
+}
